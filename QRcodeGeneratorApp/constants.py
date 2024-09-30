69fe5d0d10aaef10932b8dac0a61e8e80534aae4
Stanislav Bozhimirov <bozhimirov@gmail.com>
2024-09-30 21:41:52 +0300

    'constants'

---
+++ QRcodeGeneratorApp/constants.py
@@ -0,0 +1,44 @@
+# constants.py
+
+from qrcode.image.styles.moduledrawers import RoundedModuleDrawer, CircleModuleDrawer, GappedSquareModuleDrawer, \
+    SquareModuleDrawer, VerticalBarsDrawer, HorizontalBarsDrawer
+
+# Constants for logo paths
+LOGO_PATHS = {
+    'facebook': 'static/logos/fblogo.png',
+    'instagram': "static/logos/iglogo.png",
+    'none': None,
+    'pomarina': 'static/logos/pomarina.png',
+}
+
+# Hex color definitions
+HEX_COLORS = {
+    'facebook': {'cc': '#000000', 'ec': '#0000ff', 'bc': '#ffffff'},
+    'instagram': {'cc': '#f9ce34', 'ec': '#840cf6', 'bc': '#ffffff'},
+    'none': {'cc': '#000000', 'ec': '#000000', 'bc': '#ffffff'},
+}
+
+# Data for different logos
+DATAS = {
+    'facebook': 'www.facebook.com/profile.php?id=100087327551813',
+    'instagram': 'www.instagram.com/scouts.kreslivorel',
+    'custom': 'www.instagram.com/scouts.kreslivorel',
+    'none': 'www.instagram.com/scouts.kreslivorel'
+}
+
+# Styles for QR codes
+STYLES = {
+    'default': RoundedModuleDrawer(),
+    'circle': CircleModuleDrawer(),
+    'gapped': GappedSquareModuleDrawer(),
+    'square': SquareModuleDrawer(),
+    'vertical': VerticalBarsDrawer(),
+    'horizontal': HorizontalBarsDrawer(),
+}
+
+SIZES = {
+    'default': 140,
+    'big': 160,
+    'medium': 120,
+    'small': 100,
+}
